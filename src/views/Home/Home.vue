9dcdae4f9dcb23f72ec3abf4126cba96287e2573
henry chavez <hachch1984@gmail.com>
2020-09-15 22:13:47 -0500

    fix

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -2,67 +2,25 @@
   <div class="view-home">
 
 
-<div class="jumbotron">
-  <h1 class="display-4">Hello, world!</h1>
-  <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-  <hr class="my-4">
-  <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-  <p class="lead">
-    <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
-  </p>
-</div>
 
-    <h2>PAGINA HOME xxxxxxxxxxxxx</h2>
-    <InputNormalComponent></InputNormalComponent>
-      <InputPasswordComponent></InputPasswordComponent>
-       <InputComponent></InputComponent>
-       <CardComponent></CardComponent>
-       
-    <CardCourse />
+<div>
+
+
+<img src="../../assets/images/banner2.png"/>
+
+
+<img src="../../assets/images/iconoPachaqtec.png"/>
+
+</div>
   </div>
 </template>
 
 <script>
-import CardCourse from "./components/CardCourse";
-import CardComponent from "../../ui/Card"; 
-import InputComponent from "../../ui/Input";
-import InputNormalComponent from "../../ui/InputNormal";
-import InputPasswordComponent from "../../ui/InputPassword";
  
 
 export default {
-  name: "Home",
-  components: {
-    CardCourse,
-    InputNormalComponent,
-    InputPasswordComponent,
-    InputComponent,
-    CardComponent
-  },
-  data() {
-    return {
-      courses: [
-        {
-          id: 1,
-          name: "Desarrollo Front-End",
-          description: "",
-          itsBuy: true,
-        },
-        {
-          id: 2,
-          name: "Desarrollo Front-End",
-          description: "",
-          itsBuy: true,
-        },
-        {
-          id: 3,
-          name: "Desarrollo Front-End",
-          description: "",
-          itsBuy: true,
-        },
-      ],
-    };
-  },
+  name: "Home"
+   
 };
 </script>
 
