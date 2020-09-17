1b3b1788ff86a6de68ee8eef76370954141476e9
Mirian Quispe <mquispes@proveedor.intercorp.com.pe>
2020-09-16 23:31:02 -0500

    upload change header

--- src/views/PaymentGateway/Checkout.vue
+++ src/views/PaymentGateway/Checkout.vue
@@ -2,17 +2,61 @@
   <div>
     <Header></Header>
     <div class="view-checkout checkout">
-      <div class="checkout-card">
-        <figure>
-          <img src="@/assets/images/shippingcar1.png" />
-        </figure>
-        <div class="checkout-align">
-          <h2>Gestión de Envases, Empaques y Embalajes</h2>
-          <h3>Dto. 25%</h3>
-          <a href>Eliminar</a>
+      <div >
+        <div class="firt-section">
+          <div class="checkout-card">
+            <figure>
+              <img src="@/assets/images/shippingcar1.png" />
+            </figure>
+            <div class="checkout-align">
+              <h2>Gestión de Envases, Empaques y Embalajes</h2>
+              <h3>Dto. 25%</h3>
+              <a href>Eliminar</a>
+            </div>
+          </div>
+          <div class="amount">s/269.00</div>
+        </div>
+        <div class="firt-section">
+          <div class="checkout-card">
+            <figure>
+              <img src="@/assets/images/shippingcar1.png" />
+            </figure>
+            <div class="checkout-align">
+              <h2>Gestión de Envases, Empaques y Embalajes</h2>
+              <h3>Dto. 25%</h3>
+              <a href>Eliminar</a>
+            </div>
+          </div>
+          <div class="amount">s/269.00</div>
+        </div>
+        <div class="firt-section">
+          <div class="checkout-card">
+            <figure>
+              <img src="@/assets/images/shippingcar1.png" />
+            </figure>
+            <div class="checkout-align">
+              <h2>Gestión de Envases, Empaques y Embalajes</h2>
+              <h3>Dto. 25%</h3>
+              <a href>Eliminar</a>
+            </div>
+          </div>
+          <div class="amount">s/269.00</div>
+        </div>
+        <div class="firt-section">
+          <div class="checkout-card">
+            <figure>
+              <img src="@/assets/images/shippingcar1.png" />
+            </figure>
+            <div class="checkout-align">
+              <h2>Gestión de Envases, Empaques y Embalajes</h2>
+              <h3>Dto. 25%</h3>
+              <a href>Eliminar</a>
+            </div>
+          </div>
+          <div class="amount">s/269.00</div>
         </div>
       </div>
-      <div class="amount">s/269.00</div>
+
       <div class="pay-card">
         <div class="width">
           <div class="amount">
@@ -23,7 +67,6 @@
             <input type="text" name id placeholder="Agrega un codigo de descuento" />
             <img src="@/assets/images/send1.png" alt />
           </div>
-
           <button>Continuar</button>
         </div>
       </div>
