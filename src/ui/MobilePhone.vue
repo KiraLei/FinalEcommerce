74bd8d6822c4d9be42b9463fabe843e62b5f037d
henry chavez <hachch1984@gmail.com>
2020-09-17 23:37:20 -0500

    button1 component creation

--- src/ui/MobilePhone.vue
+++ src/ui/MobilePhone.vue
@@ -3,21 +3,25 @@
     <p>
       Postula y obtén un 10% de descuento en el programa
     </p>
-    <InputNormalComponent class="mobile-phone-input" :placeholder="Nombre" />
-    <InputNormalComponent class="mobile-phone-input" :placeholder="Nombre" />
-    <InputNormalComponent class="mobile-phone-input" :placeholder="Nombre" />
-    <InputNormalComponent class="mobile-phone-input" :placeholder="Nombre" />
+    <InputNormalComponent class="mobile-phone-control" :placeholder="Nombre" />
+    <InputNormalComponent class="mobile-phone-control" :placeholder="Nombre" />
+    <InputNormalComponent class="mobile-phone-control" :placeholder="Nombre" />
+    <InputNormalComponent class="mobile-phone-control" :placeholder="Nombre" />
+    <Button1Component class="mobile-phone-control" title="Quiero Postular" />
+    
   </div>
 </template>
 
 <script lang="ts">
 import InputNormalComponent from "./InputNormal.vue";
+import Button1Component from "./Button1.vue";
 import Vue from "vue";
 
 export default Vue.extend({
   name: "MobilePhone",
   components: {
     InputNormalComponent,
+    Button1Component,
   },
 });
 </script>
@@ -42,7 +46,7 @@ export default Vue.extend({
   display: flex;
   justify-content: center;
 }
-.mobile-phone-input {
+.mobile-phone-control {
   width: 200px;
   margin: 15px 0;
 }
