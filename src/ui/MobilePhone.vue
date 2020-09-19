e2310399dcf4bb86249bf3228aec220a5d8fca4f
Mirian Quispe <mquispes@proveedor.intercorp.com.pe>
2020-09-18 23:58:24 -0500

    phone

--- src/ui/MobilePhone.vue
+++ src/ui/MobilePhone.vue
@@ -3,10 +3,18 @@
     <p>
       Postula y obtén un 10% de descuento en el programa
     </p>
-    <InputNormalComponent class="mobile-phone-control" placeholder="Nombre" />
-    <InputNormalComponent class="mobile-phone-control" placeholder="Nombre" />
-    <InputNormalComponent class="mobile-phone-control" placeholder="Nombre" />
-    <InputNormalComponent class="mobile-phone-control" placeholder="Nombre" />
+    <InputNormalComponent class="mobile-phone-control" placeholder="NOMBRE" />
+    <InputNormalComponent class="mobile-phone-control" placeholder="CELULAR" />
+    <InputNormalComponent class="mobile-phone-control" placeholder="CORREO ELECTRONICO" />
+    
+    <select name="" id="" class="mobile-phone-control">
+      <option value="0">PROGRAMA</option>
+      <option value="" >Desarrollo Front-End</option>
+      <option value="">Desarrollo Back-End</option>
+      <option value="">Desarrollo de Aplicativos Móviles</option>
+      <option value="">Diseño de Experiencia del Usuario</option>
+      <option value="">Desarrollo de Videojuegos</option>
+    </select>
     <Button1Component class="mobile-phone-control" title="Quiero Postular" />
     
   </div>
